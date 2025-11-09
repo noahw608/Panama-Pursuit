fab3dbe0930e1d232e9d7d6eb56867be7b939a80
Alex Robinson <send2ajr@gmail.com>
2025-11-08 18:32:59 -0600

    Map
    Map

---
+++ PanamaFrontend/src/components/Map.tsx
@@ -0,0 +1,22 @@
+import { WorldMap } from "./WorldMap.tsx";
+import type { JSX } from "react";
+
+export default function Map(): JSX.Element {
+    return (
+        <section className="bg-teal py-12 flex justify-center">
+            <div className="max-w-4xl w-full px-6">
+                <h2 className="text-4xl font-semibold text-yellow-500 mb-6 text-center">
+                    Global Coverage Map
+                </h2>
+
+                 <div className="text-[#F9F9F9] text-md mb-6 opacity-90 text-center">
+                        Active Outbreaks: 42 | Countries Affected: 12
+                </div>
+
+                <div className="border-2 border-gray-300 rounded-lg overflow-hidden mx-auto w-fit shadow-md">
+                    <WorldMap />
+                </div>
+            </div>
+        </section>
+    );
+}
